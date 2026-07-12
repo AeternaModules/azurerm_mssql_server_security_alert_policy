a77ff35c2b8ad03aee29b9c1e046895b1d3fa746
Daniel Allen <dan@aeternagroup.net>
2026-07-12 21:58:53 +0100

    Update to schema version v4.80.0

--- main.tf
+++ main.tf
@@ -1,3 +1,8 @@
+data "azurerm_key_vault_secret" "storage_account_access_key" {
+  for_each     = { for k, v in var.mssql_server_security_alert_policies : k => v if v.storage_account_access_key_key_vault_id != null && v.storage_account_access_key_key_vault_secret_name != null }
+  name         = each.value.storage_account_access_key_key_vault_secret_name
+  key_vault_id = each.value.storage_account_access_key_key_vault_id
+}
 resource "azurerm_mssql_server_security_alert_policy" "mssql_server_security_alert_policies" {
   for_each = var.mssql_server_security_alert_policies
 
@@ -8,7 +13,7 @@ resource "azurerm_mssql_server_security_alert_policy" "mssql_server_security_ale
   email_account_admins       = each.value.email_account_admins
   email_addresses            = each.value.email_addresses
   retention_days             = each.value.retention_days
-  storage_account_access_key = each.value.storage_account_access_key
+  storage_account_access_key = each.value.storage_account_access_key != null ? each.value.storage_account_access_key : try(data.azurerm_key_vault_secret.storage_account_access_key[each.key].value, null)
   storage_endpoint           = each.value.storage_endpoint
 }
 
